fc1803784ad0ccb5249f81d479580ce7d11d563e
VINISHA K <109474241+kvini14@users.noreply.github.com>
2023-03-20 22:53:43 +0530

    Create Search_Insert_position.cpp

---
+++ Search_Insert_position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int size=nums.size();
+        int flag=0;
+        for (int i=0;i<size;i++){
+            if (target==nums[i])
+               return i;
+        }
+        for (int i=0;i<size;i++){
+            if(target<nums[i]){
+                return i;
+                flag=1;
+                break;
+            }
+            
+        }
+        if(flag==0)
+             return nums.size();
+        return 0;
+    }
+};
